25d4cce0c14384113f2e057a3717eb7d9a548fd8
saludxtecnologica-lang <saludxtecnologica@gmail.com>
2026-02-06 00:47:16 -0300

    optimisación SEO y confiracipon de mensaje gracias

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://strategica.cl',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
